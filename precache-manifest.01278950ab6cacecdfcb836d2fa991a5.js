9902de280169d52babef9e1e25648a4402b11e13
Wayan Danyael Eco <wayaneco29@gmail.com>
2019-10-10 01:17:40 +0800

    Updates

---
+++ precache-manifest.01278950ab6cacecdfcb836d2fa991a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19cab80678e0ca88eaaa90bfcfa8357a",
+    "url": "/contact-notes/index.html"
+  },
+  {
+    "revision": "78f6201cbc47a4a92f14",
+    "url": "/contact-notes/static/css/main.31c839d2.chunk.css"
+  },
+  {
+    "revision": "f7baed28e6799efbff1f",
+    "url": "/contact-notes/static/js/2.e0be1d81.chunk.js"
+  },
+  {
+    "revision": "78f6201cbc47a4a92f14",
+    "url": "/contact-notes/static/js/main.6ad4a75b.chunk.js"
+  },
+  {
+    "revision": "193c0d2f2e45b541d641",
+    "url": "/contact-notes/static/js/runtime-main.d0fbdead.js"
+  }
+]);
